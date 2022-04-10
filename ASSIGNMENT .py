20a2b1f8e2dbf39a7aef4ed5cc8c8ce2dd6e6bca
Saurabh sah <100861767+Sausah@users.noreply.github.com>
2022-04-10 23:56:52 +0530

    Create ASSIGNMENT .py

---
+++ ASSIGNMENT .py
@@ -0,0 +1,23 @@
+def add(num1, num2):
+	return num1 + num2
+
+# Function to subtract two numbers
+def subtract(num1, num2):
+	return num1 - num2
+
+print("Please select operation -\n" \
+		"1. Add\n" \
+		"2. Subtract\n" \
+select = int(input("Select operations form 1, 2,:"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+if select == 1:
+	print(number_1, "+", number_2, "=",
+					add(number_1, number_2))
+
+elif select == 2:
+	print(number_1, "-", number_2, "=",
+					subtract(number_1, number_2))
+else:
+	print("Invalid input")
